cbd1b9ba102bba7623f7e370a9749a6d7e9b0e14
alknas25 <160783224+alknas25@users.noreply.github.com>
2026-02-09 09:48:53 +0200

    test

---
+++ Progects Html/Project (1)/memo1.js
@@ -0,0 +1,18 @@
+document.write("Finally, thank you for using my website.<br>")
+
+// let username = "magento"
+// let number = 20
+// let number2 = 4
+// console.log(username + (number + number2))
+
+// let result = (20+8)* 2 / 4 -2 * (3+15)
+// console.log(result)
+// document.write(result)
+// console.log("hello + us ")
+// console.log("hello memo ")
+// console.log("hello memo ")
+// console.log("hello memo ")
+// console.log("hello memo ")
+// console.log("hello memo ")
+// console.log("hello memo ")
+window.alert("Welcome to my website ​")
